432e0436ec195ebc5019a55cda5f6a62f8ba7d04
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-01-27 22:06:47 -0300

    Adicionando script para mapear competidores a suas regiões

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,8 +6,10 @@ import os
 # ---------------- Constants -----------------------
 
 # paths
-OUTPUT_PATH = '../data/'
-LINKS_JSON_PATH = OUTPUT_PATH + 'links.json'
+OUTPUT_PATH = os.path.expanduser('~/maratona-sbc-analise/dados/')
+LINKS_JSON_PATH = os.path.join(OUTPUT_PATH, 'links.json')
+UNIVERSIDADES_PATH = os.path.join(OUTPUT_PATH, 'auxiliares/universidades.csv')
+REGIOES_PATH = os.path.join(OUTPUT_PATH, 'auxiliares/regioes.csv')
 
 # keys in LINKS_JSON
 COMMENT_SCOREBOARD_KEY = '_comment_scoreboard_url'
@@ -41,9 +43,9 @@ def save_as_csv(df, output_path):
 def try_load_csv(output_path, columns):
   """Try load a csv as dataframe, if it does not exist create an empty one."""
   try:
-    df_competitors = pd.read_csv(output_path)
-    print 'Loaded csv from', output_path
+    df = pd.read_csv(output_path)
+    print('Loaded csv from', output_path)
   except:
-    print 'Creating dataframe since %s does not exist' % output_path
-    df_competitors = pd.DataFrame(columns=columns)
-  return df_competitors
+    print('Creating dataframe since %s does not exist' % output_path)
+    df = pd.DataFrame(columns=columns)
+  return df
